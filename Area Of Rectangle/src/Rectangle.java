fe26398671fc73f21ec68499c54f5ce2f92fd75f
Mohamed Mohamed <mohamedm14@csp.edu>
2019-03-05 16:24:04 -0600

    Commit1

---
+++ Area Of Rectangle/src/Rectangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; // Scanner Importations
+
+/*
+ Assignment Name: Git and GitHub Extra Credit
+
+ Instructor: Dr.Cheng Thao. 
+
+ Written By: Mohamed Mohamed.
+
+ Course: CSC 422 Software Engineering
+ 
+ Written: 3/5/2019.
+
+*/
+
+
+   // Area of Rectangle class
+   public class Rectangle {
+
+	// Main Method in java
+	public static void main(String[] args) {
+
+		
+	}
+
+}
