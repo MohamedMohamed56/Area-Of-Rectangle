e4ec7528abb7e5075533ceee1f16d0bacfc50670
Mohamed Mohamed <mohamedm14@csp.edu>
2019-03-05 17:19:48 -0600

    FinalCommit

--- Area Of Rectangle/src/Rectangle.java
+++ Area Of Rectangle/src/Rectangle.java
@@ -18,16 +18,29 @@ import java.util.Scanner; // Scanner Importations
 
    public class Rectangle {
 
+	 
 	// Main Method in java
 	public static void main(String[] args) {
-		
+	
 		Scanner input = new Scanner(System.in); // Reads from keyboard
 		
 		// Finding the Length of Rectangle
+		
 		System.out.println("Please enter the length of the rectangle: ");
 		
-
+		// Obtains the double value for length
 		
+		  double length = input.nextDouble(); 
+		
+	      System.out.println("Please enter the width of the rectangle: ");
+		
+		// Obtains the double value for width
+		  double width= input.nextDouble(); 
+		  
+		  // Formula for area of rectangle
+		  double area = length * width;
+		  
+		  System.out.println("The area of rectangle:" + area); // Displays output of the formula
 	}
 
 }
